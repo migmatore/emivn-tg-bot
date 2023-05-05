dcde720ede49691f46da60aa36d623cfcb7effa3
migmatore <kazakvova201@gmail.com>
2023-05-05 11:04:18 +0300

    Update replenishment request model

--- internal/domain/replenishment_request.go
+++ internal/domain/replenishment_request.go
@@ -2,6 +2,17 @@ package domain
 
 type ReplenishmentRequest struct {
 	ReplenishmentRequestId int
-	DaimyoId               int
+	CashManagerUsername    string
+	DaimyoUsername         string
 	CardId                 int
+	Amount                 float32
+	StatusId               int
+}
+
+type ReplenishmentRequestDTO struct {
+	CashManagerUsername string
+	DaimyoUsername      string
+	CardName            string
+	Amount              float32
+	Status              string
 }
