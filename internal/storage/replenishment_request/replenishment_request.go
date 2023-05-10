0cc8aa2953f4a65a8520cf04fa11d487e6432034
migmatore <kazakvova201@gmail.com>
2023-05-10 10:18:08 +0300

    Update replenishment request storage

--- internal/storage/replenishment_request/replenishment_request.go
+++ internal/storage/replenishment_request/replenishment_request.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"emivn-tg-bot/internal/domain"
 	"emivn-tg-bot/internal/storage/psql"
+	"emivn-tg-bot/pkg/logging"
+	"github.com/migmatore/bakery-shop-api/pkg/utils"
 )
 
 type ReplenishmentRequestStorage struct {
@@ -15,5 +17,26 @@ func NewReplenishmentRequestStorage(pool psql.AtomicPoolClient) *ReplenishmentRe
 }
 
 func (s *ReplenishmentRequestStorage) Insert(ctx context.Context, replenishmentReq domain.ReplenishmentRequest) error {
+	q := `insert into replenishment_requests(cash_manager_username, daimyo_username, card_id, amount, status_id) 
+		values ($1, $2, $3, $4, $5)`
+
+	if _, err := s.pool.Exec(
+		ctx,
+		q,
+		replenishmentReq.CashManagerUsername,
+		replenishmentReq.DaimyoUsername,
+		replenishmentReq.CardId,
+		replenishmentReq.Amount,
+		replenishmentReq.StatusId,
+	); err != nil {
+		if err := utils.ParsePgError(err); err != nil {
+			logging.GetLogger(ctx).Errorf("Error: %v", err)
+			return err
+		}
+
+		logging.GetLogger(ctx).Errorf("Query error. %v", err)
+		return err
+	}
+
 	return nil
 }
