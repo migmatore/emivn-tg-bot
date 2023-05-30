c1b62a40dc86bf408530b0fadead556526a8b4cd
migmatore <kazakvova201@gmail.com>
2023-05-31 00:24:47 +0300

    Fixed some bugs and started to refactor scheduler service

--- internal/transport/bot/handler/scheduler.go
+++ internal/transport/bot/handler/scheduler.go
@@ -1,17 +1,43 @@
 package handler
 
+import (
+	"emivn-tg-bot/internal/domain"
+	"sync"
+	"time"
+)
+
 //type SchedulerService interface {
 //	Configure(listeners domain.TaskFuncsMap, sleepDuration time.Duration)
 //	Add(ctx context.Context, dto domain.TaskDTO) error
 //	Run(ctx context.Context) error
 //}
 
+const (
+	DefaultSleepDuration = 30 * time.Second
+	MinimalSleepDuration = 1 * time.Second
+)
+
 type Scheduler struct {
 	schedulerService SchedulerService
+
+	sync.RWMutex
+	listeners     domain.TaskFuncsMap
+	sleepDuration time.Duration
 }
 
 func NewScheduler(schedulerService SchedulerService) *Scheduler {
 	return &Scheduler{
 		schedulerService: schedulerService,
 	}
+}
+
+func (s *Scheduler) Configure(listeners domain.TaskFuncsMap, sleepDuration time.Duration) {
+	s.listeners = listeners
+
+	sleep := sleepDuration
+	if sleep == 0 {
+		sleep = DefaultSleepDuration
+	}
+
+	s.sleepDuration = sleep
 }
